01a70338ae514aa2603ab3b9fbc752614fed90c2
Jake Antonio <jakemattantonio@gmail.com>
2021-11-25 14:54:26 +1100

    Completed operatorBtn and evaluate functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function clearDisplay() {
+ function clearDisplay() {
     const displayValue = document.querySelector('#result');
     displayValue.textContent = "";
     firstValue = 0;
@@ -10,8 +10,19 @@ function backspace() {
     displayValue.textContent = `${displayValue.textContent.slice(0,(displayValue.textContent.length - 1))}`;
 }
 
+function evaluate(operator, firstValue, secondValue) {
+    switch(operator) {
+        case "+": return parseInt(firstValue) + parseInt(secondValue);
+        case "-": return parseInt(firstValue) - parseInt(secondValue);
+        case "×": return parseInt(firstValue) * parseInt(secondValue);
+        case "÷": return parseInt(firstValue) / parseInt(secondValue);
+    }
+}
+
 let firstValue = 0;
 let secondValue = 0;
+let operatorValue = "";
+
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener('click', () => {
@@ -21,11 +32,38 @@ buttons.forEach(button => {
         else if (button.id == 'deleteBtn') backspace();
         else if (button.id == 'numBtn') {
             if (isNaN(displayValue.textContent)) displayValue.textContent = `${button.textContent}`;
-            else displayValue.textContent = `${displayValue.textContent}${button.textContent}`;
+            else {
+                displayValue.textContent = `${displayValue.textContent}${button.textContent}`;
+            }
         }
-        else {
-            firstValue = displayValue.textContent;
-            displayValue.textContent = button.textContent;
+        else if (button.id == 'operatorBtn') {
+            if (firstValue == 0) {
+                firstValue = displayValue.textContent;
+                operatorValue = button.textContent;
+                displayValue.textContent = operatorValue;
+            }
+            else if (button.textContent == "=") {
+                if (secondValue != 0) {
+                    firstValue = evaluate(operatorValue, firstValue, secondValue);
+                    displayValue.textContent = firstValue;
+                }
+                else if ((firstValue != 0) && (isNaN(displayValue.textContent))) {
+                    firstValue = displayValue.textContent;
+                    displayValue.textContent = firstValue;
+                } 
+                else if (firstValue == 0) displayValue.textContent = button.textContent;
+                else displayValue.textContent = button.textContent;
+            }
+            else if (isNaN(displayValue.textContent)) {
+                operatorValue = button.textContent;
+                displayValue.textContent = operatorValue;
+            }
+            else {
+                secondValue = displayValue.textContent;
+                firstValue = evaluate(operatorValue, firstValue, secondValue);
+                operatorValue = button.textContent;
+                displayValue.textContent = operatorValue;
+            }
         }
     }); 
 });
